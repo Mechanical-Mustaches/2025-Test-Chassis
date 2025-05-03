99cb06d7f558d99d906db2caa06819b2dfdadd57
aebill14 <97410781+aebill14@users.noreply.github.com>
2025-05-03 12:50:25 -0500

    changed swerve ids

--- 2025-Test-Chassis/src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
+++ 2025-Test-Chassis/src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
@@ -23,6 +23,7 @@ import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -119,11 +120,7 @@ public class SwerveDriveSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-
-  }
-
-  @Override
-  public void simulationPeriodic() {
+   
   }
 
   /**
