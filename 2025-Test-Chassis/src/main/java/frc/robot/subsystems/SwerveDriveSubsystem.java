e7b4d7b415fc018e13b7052d7e03e4e7ca318337
rcbassie <rcbass27@gmail.com>
2025-05-03 13:10:43 -0500

    Revert "changed swerve ids"
    This reverts commit 99cb06d7f558d99d906db2caa06819b2dfdadd57.

--- 2025-Test-Chassis/src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
+++ 2025-Test-Chassis/src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
@@ -23,7 +23,6 @@ import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -120,7 +119,11 @@ public class SwerveDriveSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-   
+
+  }
+
+  @Override
+  public void simulationPeriodic() {
   }
 
   /**
