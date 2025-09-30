8e564047b7b949b2d26ee04bf3f7d33056f1fdba
rcbassie <rcbass27@gmail.com>
2025-09-30 15:32:19 -0500

    hbubdabadukulu

---
+++ 2025-Test-Chassis/src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ClimberSubsystem extends SubsystemBase {
+    private SparkMax climbMotor = new SparkMax(19, MotorType.kBrushless);
+
+    public void climb() {
+        climbMotor.set(0.5);
+    }
+
+    public void unclimb() {
+        climbMotor.set(-0.5);
+    }
+
+    public void stopClimb() {
+        climbMotor.set(0);
+    }
+}
